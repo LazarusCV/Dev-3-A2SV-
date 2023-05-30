d43e425dd943ff80ec16340825a088c302970ab5
Alazar Ashebir <113725494+LazarusCV@users.noreply.github.com>
2023-05-30 13:56:13 -0400

    Update Reverse Integer.py

--- Reverse Integer.py
+++ Reverse Integer.py
@@ -1,1 +1,16 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        res=0
+        sign=1
+        if x<0:
+            sign=-1
+            x=x*-1
+        while x>0:
+            rem=x%10
+            res=res*10+rem
+            x=x//10
 
+        if not -2**31<res<2**31:
+            return 0
+
+        return sign*res
